7bd8cb98c7110c938629c40aa506f000e9ca85ec
Sarthak Mukherjee <106270442+sarthak1802@users.noreply.github.com>
2023-07-08 15:31:35 +0530

    Create Calculate Delayed Arrival Time.cpp

---
+++ Calculate Delayed Arrival Time.cpp
@@ -0,0 +1,7 @@
+// Calculate Delayed Arrival Time
+class Solution {
+public:
+    int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+            return(arrivalTime+delayedTime)%24;
+    }
+};
