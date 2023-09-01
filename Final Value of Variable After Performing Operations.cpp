95016583b556ed124e533cc69411c6c5ea29fce7
Sarthak Mukherjee <106270442+sarthak1802@users.noreply.github.com>
2023-09-02 00:24:56 +0530

    Create Final Value of Variable After Performing Operations.cpp

---
+++ Final Value of Variable After Performing Operations.cpp
@@ -0,0 +1,16 @@
+// Final Value of Variable After Performing Operations
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x=0;
+        int n=operations.size();
+        while(n--)
+        {
+            if(operations[n]=="--X") --x;
+            else if(operations[n]=="X--")x--;
+            else if(operations[n]=="X++")x++;
+            else if(operations[n]=="++X")++x;
+        }
+        return x;
+    }
+};
