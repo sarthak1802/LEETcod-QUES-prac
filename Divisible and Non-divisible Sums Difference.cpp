f69076b5b7b03cd7ce8b9b733bd5219cfd13df1f
Sarthak Mukherjee <106270442+sarthak1802@users.noreply.github.com>
2023-10-11 01:38:21 +0530

    Create Divisible and Non-divisible Sums Difference.cpp

---
+++ Divisible and Non-divisible Sums Difference.cpp
@@ -0,0 +1,20 @@
+// Divisible and Non-divisible Sums Difference
+class Solution {
+public:
+    int differenceOfSums(int n, int m) {
+        int num1=0;
+        int num2=0;
+        for(int i = 1 ; i<=n ; i++ )
+        {
+            if(i % m != 0) {
+                num1 = num1+i;
+            }
+            else 
+            {
+                num2 =num2+i;
+            }
+        }
+        int ans =  num1-num2 ;
+        return ans;
+    }
+};
