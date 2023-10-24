91e208d76cd8b8851a57f76f6979b3eb2863b2b9
Sarthak Mukherjee <106270442+sarthak1802@users.noreply.github.com>
2023-10-24 12:08:42 +0530

    Create  Sum of Two Integers.cpp

---
+++ Sum of Two Integers.cpp
@@ -0,0 +1,9 @@
+//  Sum of Two Integers (Medium Level)
+class Solution {
+public:
+    int getSum(int a, int b) {
+        vector<int>ans={a,b};
+        int sum=accumulate(ans.begin(),ans.end(),0);
+        return sum;
+    }
+};
